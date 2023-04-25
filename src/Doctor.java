5b0aa992cd8f584d695238f7f770941b17863e7f
Andres Marquez <andresmr955@gmail.com>
2023-04-25 16:37:55 -0400

    Esta es la version completa de constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,21 +1,24 @@
-import javax.print.Doc;
-
 public class Doctor {
     // atributos o propiedades
     int id;
     String name;
     String speciality;
-    //Aquivisualizamos el constructor doctor aunque normalmente no esta
+
+    //Aqui visualizamos el constructor doctor aunque normalmente no esta
     Doctor(){
         System.out.println("Construyendo el objeto Doctor");
     }
 
     Doctor(String name){
-        System.out.println("El mombre del doctor asignado es " + name);
+        this.name = name;
+        System.out.println("Este es el nuevo objeto donde El mombre del doctor asignado es " + name);
     }
+
+
     //Comportamientos
     public void showName(){ // se declara un metodo
         System.out.println(name); //Nos permite imprimer lo que llamemos en este caso name
     }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ public class Main {
         System.out.println("Todo esta funcionando muy bien");
 //Tipo de Objeto-Nombre de objeto - NEW - Instanciando y creando el Obejto
         //Crea instancias, tiene el mismo nombre que la clase que se inizializa y se usa la palabra y new
-        Doctor myDoctor = new Doctor("David Marquez" );
-
-        myDoctor.name = "Andres Marquez";
+        Doctor myDoctor = new Doctor("Pedro gonzalez" );
+        myDoctor.name = "Andres Marquez Esta es el objeto ya existente";
         myDoctor.showName();
+
+
     }
 }
