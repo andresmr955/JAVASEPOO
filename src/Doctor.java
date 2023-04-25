c403886b9d927d1ab3507dd42ca19c4b04466227
Andres Marquez <andresmr955@gmail.com>
2023-04-25 15:40:34 -0400

    Esta es la explicacion del constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -9,6 +9,10 @@ public class Doctor {
     Doctor(){
         System.out.println("Construyendo el objeto Doctor");
     }
+
+    Doctor(String name){
+        System.out.println("El mombre del doctor asignado es " + name);
+    }
     //Comportamientos
     public void showName(){ // se declara un metodo
         System.out.println(name); //Nos permite imprimer lo que llamemos en este caso name
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
         System.out.println("Todo esta funcionando muy bien");
 //Tipo de Objeto-Nombre de objeto - NEW - Instanciando y creando el Obejto
         //Crea instancias, tiene el mismo nombre que la clase que se inizializa y se usa la palabra y new
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("David Marquez" );
+
         myDoctor.name = "Andres Marquez";
         myDoctor.showName();
     }
