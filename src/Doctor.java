667869340db7ce14ca40de2e06206c8b629d1396
Andres Marquez <andresmr955@gmail.com>
2023-04-25 14:54:23 -0400

    First commmit my medical appoinments

---
+++ src/Doctor.java
@@ -0,0 +1,9 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+    //Comportamientos
+    public void showName(){
+        System.out.println(name);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Todo esta funcionando muy bien");
+
+        Doctor myDoctor = new Doctor();
+        myDoctor.name = "Andres";
+        myDoctor.showName();
+    }
+}
