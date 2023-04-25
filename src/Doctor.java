3bed7bbadfd287a854b8178cad7c59edc81aeb57
Andres Marquez <andresmr955@gmail.com>
2023-04-25 15:28:45 -0400

    Este es para declarar el Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,17 @@
+import javax.print.Doc;
+
 public class Doctor {
+    // atributos o propiedades
     int id;
     String name;
     String speciality;
-    //Comportamientos
-    public void showName(){
-        System.out.println(name);
+    //Aquivisualizamos el constructor doctor aunque normalmente no esta
+    Doctor(){
+        System.out.println("Construyendo el objeto Doctor");
     }
+    //Comportamientos
+    public void showName(){ // se declara un metodo
+        System.out.println(name); //Nos permite imprimer lo que llamemos en este caso name
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Todo esta funcionando muy bien");
-
+//Tipo de Objeto-Nombre de objeto - NEW - Instanciando y creando el Obejto
+        //Crea instancias, tiene el mismo nombre que la clase que se inizializa y se usa la palabra y new
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Andres Marquez";
         myDoctor.showName();
