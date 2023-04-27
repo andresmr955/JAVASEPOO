b813815c943e0672db88c44e58f6edbbccbeeac1
Andres Marquez <andresmr955@gmail.com>
2023-04-27 09:04:19 -0400

    Este es el ejemplo del contador static

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,12 +1,13 @@
 public class Doctor {
     // atributos o propiedades
-    int id;
+    static int id = 0; //Autoincrement, variable estatica
     String name;
     String speciality;
 
     //Aqui visualizamos el constructor doctor aunque normalmente no esta
     Doctor(){
         System.out.println("Construyendo el objeto Doctor");
+        id++;
     }
 
     Doctor(String name){
@@ -16,9 +17,11 @@ public class Doctor {
 
 
     //Comportamientos
+    public void showId(){ // se declara un metodo
+        System.out.println("ID Doctor: " + Doctor.id); //Nos permite imprimer lo que llamemos en este caso name
+    }
     public void showName(){ // se declara un metodo
         System.out.println(name); //Nos permite imprimer lo que llamemos en este caso name
     }
-
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import javax.print.Doc;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Todo esta funcionando muy bien");
 //Tipo de Objeto-Nombre de objeto - NEW - Instanciando y creando el Obejto
         //Crea instancias, tiene el mismo nombre que la clase que se inizializa y se usa la palabra y new
-        Doctor myDoctor = new Doctor("Pedro gonzalez" );
+        Doctor myDoctor = new Doctor(  ); //Se va a cambiar para que siempre que se cree un objeto de tipo doctor, siempre lo este deifiniendo un nuevo identificador
         myDoctor.name = "Andres Marquez Esta es el objeto ya existente";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor.id++;
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
 
 
     }
