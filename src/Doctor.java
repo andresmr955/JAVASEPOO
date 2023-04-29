f555af03a35e256f7a3445ab37ee15dbcc1ca493
Andres Marquez <andresmr955@gmail.com>
2023-04-29 19:22:18 -0400

    Modificadores de acceso

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,19 +1,23 @@
 public class Doctor {
     // atributos o propiedades
-    static int id;
+    static int id = 0;// autoincrement
     String name;
+    String email;
     String speciality;
 
     //Aqui visualizamos el constructor doctor aunque normalmente no esta
     Doctor(){
         System.out.println("Construyendo el objeto Doctor");
-        id++;
     }
 
-    Doctor(String name){
-        this.name = name;
-        System.out.println("Este es el nuevo objeto donde El mombre del doctor asignado es " + name);
-    }
+        Doctor(String name, String speciality){
+            System.out.println("El nombre del Doctor asignado es: " + name);
+            id++;
+            this.name = name;//"Andres Marquez""andresmr955@gmail.com"
+            this.speciality = speciality;
+
+        }
+
 
 
     //Comportamientos
--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,31 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Andres Marquez";
-        myDoctor.showName();
-        myDoctor.showId();
-        //System.out.println(Doctor.id);
+        Doctor myDoctor = new Doctor("Andres","Programador ");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
 
-        Doctor myDoctorAnd = new Doctor();
-        myDoctor.name = "Angie Bareno";
-        myDoctor.showName();
-        myDoctor.showId();
-        showMenu();
+        int i = 0;
+        int b = 2;
+        b = i;
+        //b=0
 
+        Patient patient = new Patient("Andres", "Andres@gmail.oom");
+
+        Patient patient2 = new Patient("Alejandra", "alejandra@gmail.oom");
+
+        System.out.println("Este es el paciente 1 " + patient.getName());
+        System.out.println("Este es el paciente 2 " + patient2.getName());
+        patient2 = patient;
+
+        System.out.println("Este es el nuevo paciente 1 " + patient.getName());
+        System.out.println( "Este es el paciente 1 asignado al dos " + patient2.getName());
+
+        //Aqui se aprendio como encapsular y autilizar los modificadores de acceso de acceso como private, protected, public,default
+        patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());
     }
 }
