0d7efd681503370e84aefb9a2a8642235e5b8a9a
Andres Marquez <andresmr955@gmail.com>
2023-04-29 21:35:56 -0400

    Intro Clases anidadas

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,14 @@
+import java.util.Date;
+import java.util.ArrayList;
 public class Doctor {
     // atributos o propiedades
     static int id = 0;// autoincrement
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
+
+
+
 
     //Aqui visualizamos el constructor doctor aunque normalmente no esta
     Doctor(){
@@ -28,5 +33,44 @@ public class Doctor {
     public void showId(){
         System.out.println("ID Doctor: " + id);
     }
+    //Available appointment
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppoinment(Date date, String time){}
+
+    public static class AvailableAppointment{
+        private int id_availableAppointment;
+        private Date date;
+        private String time;
+
+        public AvailableAppointment(Date date, String time){
+            this.date = date;
+            this.time = time;
+        }
+    }
+
+        public int getId_availableAppointment() {
+            return id_availableAppointment;
+        }
+
+        public void setId_availableAppointment(int id_availableAppointment) {
+            this.id_availableAppointment = id_availableAppointment;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
 
 }
