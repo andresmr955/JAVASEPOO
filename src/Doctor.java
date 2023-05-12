19799a1253303aeb6bdf9186ac5a597b68ac529e
Andres Marquez <andresmr955@gmail.com>
2023-05-11 21:27:05 -0400

    Comenzando enumerations

--- src/Doctor.java
+++ src/Doctor.java
@@ -70,4 +70,31 @@ public class Doctor{
         }
 
     }
+
+    //enumerations
+    public enum Day {
+        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+    }
+
+    Day day;
+    switch (day) {
+        case MONDAY:
+            System.out.println("Mondays are good.");
+            break;
+        case FRIDAY:
+            System.out.println("Fridays are nice");
+            break;
+        case SATURDAY: case: SUNDAY:
+        System.out.println("Weekends are the best");
+            break;
+        default:
+            System.out.println("Midweek are so-so");
+            break;
+
+    }
+    Day.MONDAY;
+    Day.FRIDAY;
+    Day.SATURDAY
+
+    }
 }
